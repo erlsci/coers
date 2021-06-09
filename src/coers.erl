be907ddb5050e7350f711a096a5ae5f68d5fc63f
Duncan McGreggor <oubiwann@gmail.com>
2021-06-09 16:32:07 -0500

    Consistent formatting  (Emacs).

--- src/coers.erl
+++ src/coers.erl
@@ -4,27 +4,27 @@
 
 %% API of Coers
 -export([
-  value/1,
-  error/1,
-  has_error/1,
-  is_ascii_char/1,
-  maybe_string/1,
-  to_string/1,
-  to_string/2,
-  of_string/1,
-  of_string/2,
-  numeric_align/1,
-  to_int/1,
-  to_int/2,
-  to_float/1,
-  to_float/2,
-  to_atom/1,
-  to_atom/2,
-  to_bool/1,
-  to_bool/2,
-  to_rational/1,
-  to_rational/2
-]).
+         value/1,
+         error/1,
+         has_error/1,
+         is_ascii_char/1,
+         maybe_string/1,
+         to_string/1,
+         to_string/2,
+         of_string/1,
+         of_string/2,
+         numeric_align/1,
+         to_int/1,
+         to_int/2,
+         to_float/1,
+         to_float/2,
+         to_atom/1,
+         to_atom/2,
+         to_bool/1,
+         to_bool/2,
+         to_rational/1,
+         to_rational/2
+        ]).
 
 -include_lib("results/include/results.hrl").
 
@@ -46,29 +46,29 @@ has_error(Result) ->
 %% @doc determine if an integer is a potential Ascii Char
 -spec is_ascii_char(integer()) -> boolean().
 is_ascii_char(X) when is_integer(X) ->
-  (X >= 32) and (X < 127);
+    (X >= 32) and (X < 127);
 is_ascii_char([H]) ->
-  is_ascii_char(H);
+    is_ascii_char(H);
 is_ascii_char(_) ->
-  false.
+    false.
 
 %% @doc check if a list is maybe a string
 -spec maybe_string(list()) -> boolean().
 maybe_string(List) when is_list(List) ->
-  lists:all(fun is_ascii_char/1, List);
+    lists:all(fun is_ascii_char/1, List);
 maybe_string(_) -> false.
 
 %% @doc try to coerce term into string
 -spec to_string((binary() | [any()])) -> result().
 to_string(Term) when is_bitstring(Term) ->
-  List = binary_to_list(Term),
-  to_string(List);
+    List = binary_to_list(Term),
+    to_string(List);
 to_string(Term) ->
-  case maybe_string(Term) of
-    true -> results:new(Term);
-    false ->
-      List = io_lib:format("~p", [Term]),
-      results:new(lists:flatten(List))
+    case maybe_string(Term) of
+        true -> results:new(Term);
+        false ->
+            List = io_lib:format("~p", [Term]),
+            results:new(lists:flatten(List))
     end.
 
 %% @doc Replace value if coercion failed
@@ -82,31 +82,31 @@ to_string(Term, Default) ->
 %% @doc an ugly and magic coercion from string to term()
 -spec of_string(string()) -> result().
 of_string(String) ->
-  {ok, Regexp} = re:compile("^.+(\\,|\\;|\\.)$"),
+    {ok, Regexp} = re:compile("^.+(\\,|\\;|\\.)$"),
     S =
-      case re:run(String, Regexp) of
-        {match, [_, {Offset, _}]} ->
-          Substring = string:substr(String, 1, Offset -1),
-          Substring ++ ".";
-        _ -> String ++ "."
-      end,
+        case re:run(String, Regexp) of
+            {match, [_, {Offset, _}]} ->
+                Substring = string:substr(String, 1, Offset -1),
+                Substring ++ ".";
+            _ -> String ++ "."
+        end,
     case erl_scan:string(S) of
-      {ok, Tokens, _} ->
-        case erl_parse:parse_exprs(Tokens) of
-          {ok, Exprs} ->
-            {value, Val, []} = erl_eval:exprs(Exprs, []),
-            results:new(Val);
-          {error, {_, erl_parse, _}} ->
-            %% TODO extract the error message and add to new_error
-            format_error_msg(erl_parse, "Could not convert string ~p", [String]);
-          {error, {Err, A, B}} ->
+        {ok, Tokens, _} ->
+            case erl_parse:parse_exprs(Tokens) of
+                {ok, Exprs} ->
+                    {value, Val, []} = erl_eval:exprs(Exprs, []),
+                    results:new(Val);
+                {error, {_, erl_parse, _}} ->
+                    %% TODO extract the error message and add to new_error
+                    format_error_msg(erl_parse, "Could not convert string ~p", [String]);
+                {error, {Err, A, B}} ->
+                    %% TODO extract the error message and add to new_error
+                    format_error_msg(Err, "Could not convert string ~p, ~p, ~p", [String, A, B])
+            end;
+        {error, {Err, A, B}, _} ->
             %% TODO extract the error message and add to new_error
             format_error_msg(Err, "Could not convert string ~p, ~p, ~p", [String, A, B])
-        end;
-      {error, {Err, A, B}, _} ->
-        %% TODO extract the error message and add to new_error
-        format_error_msg(Err, "Could not convert string ~p, ~p, ~p", [String, A, B])
-  end.
+    end.
 
 %% @doc try coercion or define a default value the suceeded flag is preserved
 -spec of_string(string(), term()) -> result().
@@ -118,41 +118,41 @@ of_string(Term, Default) ->
 %% @doc numeric alignement of a string (float of int)
 -spec numeric_align(string()) -> atom().
 numeric_align(String) ->
-  {ok, RatioRegex} = re:compile(?RATIO_REGEX),
-  case re:run(String, RatioRegex) of
-    {match, _} -> rational;
-    _ -> numeric_alignt_int_float(String)
-  end.
+    {ok, RatioRegex} = re:compile(?RATIO_REGEX),
+    case re:run(String, RatioRegex) of
+        {match, _} -> rational;
+        _ -> numeric_alignt_int_float(String)
+    end.
 
 -spec numeric_alignt_int_float(string()) -> atom().
 numeric_alignt_int_float(String) ->
-  {ok, Regexp} = re:compile(?NUM_REGEX),
-  case re:run(String, Regexp) of
-    {match, [_, _]} -> integer;
-    {match, [_, _, _]} -> float;
-    {match, [_, _, _, _]} -> float;
-    _ -> any
-  end.
+    {ok, Regexp} = re:compile(?NUM_REGEX),
+    case re:run(String, Regexp) of
+        {match, [_, _]} -> integer;
+        {match, [_, _, _]} -> float;
+        {match, [_, _, _, _]} -> float;
+        _ -> any
+    end.
 
 %% @doc try to coerce a term to an integer
 -spec to_int(term()) -> result().
 to_int(Obj) when is_integer(Obj) -> results:new(Obj);
 to_int(Obj) when is_bitstring(Obj) -> to_int(binary_to_list(Obj));
 to_int(Obj) when is_list(Obj)    ->
-  try list_to_integer(Obj) of
-  Val    -> results:new(Val)
-  catch error:Err ->
-    case numeric_align(Obj) of
-      float -> to_int(list_to_float(Obj));
-      Type     -> format_error_msg(Err, "Could not convert ~p (type ~p) to int", [Obj, Type])
-    end
-  end;
+    try list_to_integer(Obj) of
+        Val    -> results:new(Val)
+    catch error:Err ->
+            case numeric_align(Obj) of
+                float -> to_int(list_to_float(Obj));
+                Type     -> format_error_msg(Err, "Could not convert ~p (type ~p) to int", [Obj, Type])
+            end
+    end;
 to_int(Obj) when is_atom(Obj)     ->
-  try Soft = atom_to_list(Obj), to_int(Soft) of
-  Result     -> Result
-  catch  error:Err ->
-    format_error_msg(Err, "Could not convert ~p to int", [Obj])
-  end;
+    try Soft = atom_to_list(Obj), to_int(Soft) of
+        Result     -> Result
+    catch  error:Err ->
+            format_error_msg(Err, "Could not convert ~p to int", [Obj])
+    end;
 to_int(Obj) -> format_error_msg(error, "Could not convert ~p to int", [Obj]).
 
 %% @doc try coercion or define a default value
@@ -169,20 +169,20 @@ to_int(Term, Default) ->
 to_float(Obj) when is_float(Obj)     -> results:new(Obj);
 to_float(Obj) when is_bitstring(Obj) -> to_float(binary_to_list(Obj));
 to_float(Obj) when is_list(Obj)      ->
-  try list_to_float(Obj) of
-  Val     -> results:new(Val)
-  catch error:Err ->
-    case numeric_align(Obj) of
-      integer -> to_float(list_to_integer(Obj));
-      Type     -> format_error_msg(Err, "Could not convert ~p (type ~p) to float", [Obj, Type])
-    end
-  end;
+    try list_to_float(Obj) of
+        Val     -> results:new(Val)
+    catch error:Err ->
+            case numeric_align(Obj) of
+                integer -> to_float(list_to_integer(Obj));
+                Type     -> format_error_msg(Err, "Could not convert ~p (type ~p) to float", [Obj, Type])
+            end
+    end;
 to_float(Obj) when is_atom(Obj) ->
-  try Obj2 = atom_to_list(Obj), to_float(Obj2) of
-  Result -> Result
-  catch error:Err ->
-    format_error_msg(Err, "Could not convert ~p to flost", [Obj])
-  end;
+    try Obj2 = atom_to_list(Obj), to_float(Obj2) of
+        Result -> Result
+    catch error:Err ->
+            format_error_msg(Err, "Could not convert ~p to flost", [Obj])
+    end;
 to_float(Obj) ->
     format_error_msg(error, "Could not convert ~p to flost", [Obj]).
 
@@ -197,13 +197,13 @@ to_float(Term, Default) ->
 -spec to_atom(term()) -> result().
 to_atom(Obj) when is_atom(Obj)  -> results:new(Obj);
 to_atom(Obj) when is_list(Obj)  ->
-  try list_to_atom(Obj) of
-  Val     -> results:new(Val)
-  catch Err:_ -> results:new_error(Err)
-  end;
+    try list_to_atom(Obj) of
+        Val     -> results:new(Val)
+    catch Err:_ -> results:new_error(Err)
+    end;
 to_atom(Obj) ->
-  Pred = to_string(Obj),
-  to_atom(results:value(Pred)).
+    Pred = to_string(Obj),
+    to_atom(results:value(Pred)).
 
 %% @doc try coercion or define a default value the suceeded flag is preserved
 -spec to_atom(term(), term()) -> result().
@@ -215,16 +215,16 @@ to_atom(Term, Default) ->
 %% @doc try to coerce a term to a boolean
 -spec to_bool(term()) -> result().
 to_bool(Obj) when is_atom(Obj) ->
-  results:new(not (Obj == false));
+    results:new(not (Obj == false));
 to_bool(Obj) when is_list(Obj) ->
-  case string:to_lower(Obj) of
-    "true"   -> results:new(true);
-    "false"  -> results:new(false);
-    _        -> results:new(true)
-  end;
+    case string:to_lower(Obj) of
+        "true"   -> results:new(true);
+        "false"  -> results:new(false);
+        _        -> results:new(true)
+    end;
 to_bool(X) when is_bitstring(X) ->
-  Pred = to_string(X),
-  to_bool(results:value(Pred));
+    Pred = to_string(X),
+    to_bool(results:value(Pred));
 to_bool(0)   -> results:new(false);
 to_bool(0.0) -> results:new(false);
 to_bool(1)   -> results:new(true);
@@ -234,7 +234,7 @@ to_bool(_)   -> results:new(true).
 %% @doc try coercion or define a default value the suceeded flag is preserved
 -spec to_bool(term(), term()) -> result().
 to_bool(Term, Default) ->
-  results:attempt(to_bool(Term), Default).
+    results:attempt(to_bool(Term), Default).
 
 -spec to_rational(term()) -> result().
 to_rational(Obj) when is_list(Obj) ->
--- test/coers_test.erl
+++ test/coers_test.erl
@@ -5,115 +5,115 @@
 
 %% Test for coers:is_ascii_char
 is_ascii_char_test() ->
-  Flag = lists:all(
-    fun coers:is_ascii_char/1,
-    lists:seq(32, 126)
-  ),
-  ?assert(Flag),
-  ?assert(coers:is_ascii_char("A")),
-  ?assertNot(coers:is_ascii_char(222)),
-  ?assertNot(coers:is_ascii_char(a)).
+    Flag = lists:all(
+             fun coers:is_ascii_char/1,
+             lists:seq(32, 126)
+            ),
+    ?assert(Flag),
+    ?assert(coers:is_ascii_char("A")),
+    ?assertNot(coers:is_ascii_char(222)),
+    ?assertNot(coers:is_ascii_char(a)).
 
 %% Test for maybe_string
 maybe_string_test() ->
-  ?assert(coers:maybe_string("")),
-  ?assert(coers:maybe_string("Hello")),
-  ?assert(coers:maybe_string([32, 33, 34])),
-  ?assertNot(coers:maybe_string(42)),
-  ?assertNot(coers:maybe_string([0,1])),
-  ?assertNot(coers:maybe_string(atom)),
-  ?assertNot(coers:maybe_string(42.0)).
+    ?assert(coers:maybe_string("")),
+    ?assert(coers:maybe_string("Hello")),
+    ?assert(coers:maybe_string([32, 33, 34])),
+    ?assertNot(coers:maybe_string(42)),
+    ?assertNot(coers:maybe_string([0,1])),
+    ?assertNot(coers:maybe_string(atom)),
+    ?assertNot(coers:maybe_string(42.0)).
 
 %% Test for to_string coersion
 to_string_test() ->
-  ?assertEqual("coers", coers:value(coers:to_string("coers"))),
-  ?assertEqual("coers", coers:value(coers:to_string(coers))),
-  ?assertEqual("5/16", coers:value(coers:to_string("5/16"))),
-  ?assertEqual("", coers:value(coers:to_string([]))),
-  ?assertEqual("42", coers:value(coers:to_string(42))),
-  ?assertEqual("coers", coers:value(coers:to_string(<<"coers">>))),
-  ?assertEqual("42.0", coers:value(coers:to_string(42.0))).
+    ?assertEqual("coers", coers:value(coers:to_string("coers"))),
+    ?assertEqual("coers", coers:value(coers:to_string(coers))),
+    ?assertEqual("5/16", coers:value(coers:to_string("5/16"))),
+    ?assertEqual("", coers:value(coers:to_string([]))),
+    ?assertEqual("42", coers:value(coers:to_string(42))),
+    ?assertEqual("coers", coers:value(coers:to_string(<<"coers">>))),
+    ?assertEqual("42.0", coers:value(coers:to_string(42.0))).
 
 %% Test suits for magic coersion
 of_string_atomic_test() ->
-  R = coers:of_string("an_atom"),
-  ?assertEqual(an_atom, coers:value(R)).
+    R = coers:of_string("an_atom"),
+    ?assertEqual(an_atom, coers:value(R)).
 
 of_string_list_test() ->
-  R = coers:of_string("[1,2,3,4]"),
-  ?assertEqual([1,2,3,4], coers:value(R)).
+    R = coers:of_string("[1,2,3,4]"),
+    ?assertEqual([1,2,3,4], coers:value(R)).
 
 of_string_numeric_test() ->
-  R = coers:of_string("{45, 45.3}"),
-  ?assertEqual({45, 45.3}, coers:value(R)).
+    R = coers:of_string("{45, 45.3}"),
+    ?assertEqual({45, 45.3}, coers:value(R)).
 
 of_string_bitstring_test() ->
-  R = coers:of_string("<<\"foo\">>"),
-  ?assertEqual( <<"foo">>, coers:value(R)).
+    R = coers:of_string("<<\"foo\">>"),
+    ?assertEqual( <<"foo">>, coers:value(R)).
 
 of_string_errors_test() ->
     ?assertEqual({erl_parse,"Could not convert string \"{45\""}, coers:error(coers:of_string("{45"))).
 
 of_string_defaults_test() ->
-  R = coers:of_string("<<43", "<<\"42\">>"),
-  U = coers:of_string("foo", "bar"),
-  ?assertEqual(<<"42">>, coers:value(R)),
-  ?assertEqual(foo, coers:value(U)).
+    R = coers:of_string("<<43", "<<\"42\">>"),
+    U = coers:of_string("foo", "bar"),
+    ?assertEqual(<<"42">>, coers:value(R)),
+    ?assertEqual(foo, coers:value(U)).
 
 to_int_test() ->
-  F = fun(X) -> coers:value(coers:to_int(X)) end,
-  G = fun(X, Y) -> coers:value(coers:to_int(X, Y)) end,
-  ?assertEqual(1000, F("1000")),
-  ?assertEqual(444, F(444)),
-  ?assertEqual(123, F('123')),
-  ?assertEqual(123, F("+0123")),
-  ?assertEqual(-123, F("-0123")),
-  ?assertEqual(23, F(<<"23">>)).
+    F = fun(X) -> coers:value(coers:to_int(X)) end,
+    G = fun(X, Y) -> coers:value(coers:to_int(X, Y)) end,
+    ?assertEqual(1000, F("1000")),
+    ?assertEqual(444, F(444)),
+    ?assertEqual(123, F('123')),
+    ?assertEqual(123, F("+0123")),
+    ?assertEqual(-123, F("-0123")),
+    ?assertEqual(23, F(<<"23">>)).
 
 to_int_errors_test() ->
-  ?assertEqual({error,"Could not convert 111.2 to int"}, coers:error(coers:to_int(111.2))),
-  ?assertEqual({badarg,"Could not convert \"5/16\" (type rational) to int"}, coers:error(coers:to_int("5/16"))),
-  ?assertEqual({badarg,"Could not convert \"-0/16\" (type rational) to int"}, coers:error(coers:to_int("-0/16"))),
-  ?assertEqual({badarg,"Could not convert \"+5/0\" (type rational) to int"}, coers:error(coers:to_int("+5/0"))).
+    ?assertEqual({error,"Could not convert 111.2 to int"}, coers:error(coers:to_int(111.2))),
+    ?assertEqual({badarg,"Could not convert \"5/16\" (type rational) to int"}, coers:error(coers:to_int("5/16"))),
+    ?assertEqual({badarg,"Could not convert \"-0/16\" (type rational) to int"}, coers:error(coers:to_int("-0/16"))),
+    ?assertEqual({badarg,"Could not convert \"+5/0\" (type rational) to int"}, coers:error(coers:to_int("+5/0"))).
 
 to_int_defaults_test() ->
-  ?assertEqual(111, coers:value(coers:to_int("foo", 111))).
+    ?assertEqual(111, coers:value(coers:to_int("foo", 111))).
 
 to_float_test() ->
-  ?assertEqual(123.23, coers:value(coers:to_float('123.23'))),
-  ?assertEqual(111.2, coers:value(coers:to_float(111.2))),
-  ?assertEqual(123.7654, coers:value(coers:to_float("+0123.7654"))),
-  ?assertEqual(-123.7654, coers:value(coers:to_float("-0123.7654"))),
-  ?assertEqual(23.78, coers:value(coers:to_float(<<"23.78">>))).
+    ?assertEqual(123.23, coers:value(coers:to_float('123.23'))),
+    ?assertEqual(111.2, coers:value(coers:to_float(111.2))),
+    ?assertEqual(123.7654, coers:value(coers:to_float("+0123.7654"))),
+    ?assertEqual(-123.7654, coers:value(coers:to_float("-0123.7654"))),
+    ?assertEqual(23.78, coers:value(coers:to_float(<<"23.78">>))).
 
 to_float_errors_test() ->
     ?assertEqual({error,"Could not convert 1000 to flost"}, coers:error(coers:to_float("1000"))),
     ?assertEqual({error,"Could not convert 444 to flost"}, coers:error(coers:to_float(444))).
-    
+
 to_float_defaults_test() ->
     ?assertEqual(444.0, coers:value(coers:to_float("444", "444.0"))),
     ?assertEqual(444.0, coers:value(coers:to_float(444, '444.0'))).
 
 to_atom_test() ->
-  F = fun(X) -> coers:value(coers:to_atom(X)) end,
-  ?assertEqual(foo, F(foo)),
-  ?assertEqual(foo, F("foo")),
-  ?assertEqual(foo, F(<<"foo">>)),
-  ?assertEqual('222.987', F(222.987)).
+    F = fun(X) -> coers:value(coers:to_atom(X)) end,
+    ?assertEqual(foo, F(foo)),
+    ?assertEqual(foo, F("foo")),
+    ?assertEqual(foo, F(<<"foo">>)),
+    ?assertEqual('222.987', F(222.987)).
 
 to_bool_test() ->
-  F = fun(X) -> coers:value(coers:to_bool(X)) end,
-  ?assert(F("true")),
-  ?assert(F(true)),
-  ?assert(F(1)),
-  ?assert(F(1.0)),
-  ?assert(F("TrUe")),
-  ?assert(F(trUe)),
-  ?assertNot(F(false)),
-  ?assertNot(F("False")),
-  ?assertNot(F(<<"false">>)),
-  ?assertNot(F(0)),
-  ?assertNot(F(0.0)).
+    F = fun(X) -> coers:value(coers:to_bool(X)) end,
+    ?assert(F("true")),
+    ?assert(F(true)),
+    ?assert(F(1)),
+    ?assert(F(1.0)),
+    ?assert(F("TrUe")),
+    ?assert(F(trUe)),
+    ?assertNot(F(false)),
+    ?assertNot(F("False")),
+    ?assertNot(F(<<"false">>)),
+    ?assertNot(F(0)),
+    ?assertNot(F(0.0)).
 
 numeric_align_test() ->
     ?assertEqual(integer, coers:numeric_align("516")),
